6d8a1bd765bb477c12885f3840aa39b9db4cf582
Martijn Vaes <martijnvaes01@gmail.com>
2026-09-22 21:59:48 +0200

    v477

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* Grand Tour v469. Written by standalone.js; edit it there. */
+/* Grand Tour v477. Written by standalone.js; edit it there. */
 var CACHE="grandtour";
 var KEEP=["./","index.html","manifest.webmanifest","icon-180.png","icon-512.png","assets/flags.json","assets/coast.json","fonts/abrilfatface.woff2","fonts/caveat.woff2","fonts/oswald.woff2","fonts/specialelite.woff2","fonts/staatliches.woff2"];
 self.addEventListener("install",function(e){
